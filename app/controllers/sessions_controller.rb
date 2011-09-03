d3c11490e0dc26512adf17fbc26d152ec7072185
Tony Ng <tonyng222@gmail.com>
2011-09-02 21:04:16 -0700

    Done with user edit/update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   def new
     @title = "Sign in"
   end
-
-   def create
+ 
+  def create
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
     if user.nil?
@@ -13,10 +13,10 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-	  redirect_to user
+      redirect_back_or user
     end
   end
-
+  
   def destroy
     sign_out
 	redirect_to root_path
